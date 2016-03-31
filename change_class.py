5b06ab85def5d03b3c1e07b98be209878ff24118
Daniel Bridera <danielbridera@gmail.com>
2016-03-31 09:20:23 +0200

    Agregados scripts casi finales

--- change_class.py
+++ change_class.py
@@ -23,6 +23,7 @@ class ChangeImage(object):
 
 		self.imgBef = gdal.Open(self.imgBef).ReadAsArray().astype('float')
 		self.imgAft = gdal.Open(self.imgAft).ReadAsArray().astype('float')
+
 		self.bands , self.x, self.y = self.imgAft.shape
 		self.coef = sqrt(factorial(self.bands)/ (2* factorial(self.bands - 2)))
 	def getNDIs(self, matrix):
@@ -40,10 +41,10 @@ class ChangeImage(object):
 
 			img1 = matrix[b1,:,:]
 			img2 = matrix[b2,:,:]
-			with np.errstate(invalid='ignore',divide='ignore'):
-				res[i,:,:]= (img1 - img2) / (img1 + img2)
+			#with np.errstate(invalid='ignore',divide='ignore'):
+			res[i,:,:]= (img1 - img2) / (img1 + img2)
 
-		res[~np.isfinite(res)]= 0
+		#res[~np.isfinite(res)]= 0
 
 		return res
 
@@ -57,7 +58,8 @@ class ChangeImage(object):
 		elif source =='ndi':
 			self.diff = getNDIs(self.imgAft) - getNDIs(self.imgBef)
 			self.bands , self.x, self.y = self.diff.shape
-			self.coef = sqrt(factorial(self.bands)/ (2* factorial(self.bands - 2)))
+			self.coef = sqrt(self.bands)
+
 
 	def getMagnitudeMatrix(self):
 		pre = self.diff**2
@@ -70,9 +72,8 @@ class ChangeImage(object):
 
 		numerator = np.sum(self.diff, axis=0)
 		#coef = sqrt(factorial(self.bands)/ (2* factorial(self.bands - 2)))
-		with np.errstate(invalid='ignore',divide='ignore'):
-			self.alpha = numerator/ (self.coef * self.ros)
-			self.alpha[~np.isfinite(self.alpha)] = 0
+		#with np.errstate(invalid='ignore',divide='ignore'):
+		self.alpha = numerator/ (self.coef * self.ros)
 
 	def getLimits(self, alpha):
 		middle = (self.ros.max() - self.ros.min())/2
@@ -109,14 +110,26 @@ class ChangeImage(object):
 
 		Pn_Pxn =Pnc * Pxn
 		Pc_Pxc = Pc* Pxc
-		Px = Pn_Pxn + Pc_Pxc
+		#print Pn_Pxn , Pc_Pxc
 
-		return Pn_Pxn/Px , Pc_Pxc/Px
+		Px = Pn_Pxn + Pc_Pxc
+		ratio1 = Pn_Pxn/Px
+		ratio2 = Pc_Pxc/Px
+			
+		"""
+		with np.errstate(invalid='ignore',divide='ignore'):
+			ratio1 = Pn_Pxn/Px
+			ratio2 = Pc_Pxc/Px
+			ratio1[~np.isfinite(ratio1)] = 0
+			ratio2[~np.isfinite(ratio2)] = 0
+		"""
+		return  ratio1, ratio2
 
 	def equation1(self,ratioPn, ratioPc):
 		# esto me viene, para no calcularlo de nuevo!!!
 		#newPn , newPc = computeRatio(Pc, Pnc, mu_c, sigma_c, mu_nc, sigma_nc)
 		denominator = (self.x * self.y)
+		#denominator = 6262
 		return np.sum(ratioPn)/denominator, np.sum(ratioPc)/denominator
 
 	def equation2(self,ratioPn, ratioPc):
@@ -128,56 +141,154 @@ class ChangeImage(object):
 	def equation3(self,ratioPn, ratioPc, mu_nc,  mu_c):
 		num_n = np.sum(ratioPn * ((self.ros - mu_nc)**2))
 		num_c = np.sum(ratioPc * ((self.ros - mu_c)**2))
-
+		
+		
 		return num_n/np.sum(ratioPn), num_c/np.sum(ratioPc)
 
 start_time = time.clock()
 
-t = ChangeImage("img/recorte1","img/recorte2")
+img1 = "RECORTES/400_mix"
+img2 = "RECORTES/ruido_normal_cultivos_agua_agua"
+t = ChangeImage(img1,img2)
+
 t.load()
 t.imageDiff(source='ndi')
 t.getMagnitudeMatrix()
 t.getAlphaMatrix()
-t.fixRos()
-t.getLimits(0.03)
-t.ros = t.ros.flatten()
 
+t.fixRos()
+
+print t.alpha.min(), t.alpha.max()
+
+
+#cambios = t.alpha[np.where(t.ros >0.30420492642543551)]
+
+t.alpha[np.where(t.alpha >-0.825)] = 2
+t.alpha[np.where(t.alpha <=-0.825)] = 1
+t.alpha[np.where(t.ros <=0.30420492642543551)] = 0
+
+gdal_array.SaveArray(t.alpha.astype("int"), "clasificada_AGUA_AGUA", "GTiff", gdal.Open(img1))
+
+exit()
+
+
+
+
+
+#t.getAlphaMatrix()
+#print t.ros[120:190,120:190]
+#print ""
+#print t.ros[300:399,300:399]
+
+"""
+print "ROS extremos"
+
+p = t.ros[np.where(t.ros !=0)]
+
+#print p.min()
+t.ros[np.where(t.ros ==0)]= p.min()
+
+print t.ros.min(), t.ros.max()
+
+
+#print t.ros[np.where(t.ros >0.6)].shape
+
+print "Medio"
+print (-t.ros.min() + t.ros.max())/2
+
+t.getLimits(0.2)
+
+print "limites"
+"""
+
+"""
+import matplotlib.pyplot as plt
+
+plt.hist(t.ros, bins=[0,0.1,0.2,0.3,0.4,0.5,0.6])
+
+plt.show()
+exit()
+"""
+"""
+t.ros[np.where(t.ros >0.30420492642543551)] = 1	
+t.ros[np.where(t.ros <= 0.30420492642543551)] = 0
+
+gdal_array.SaveArray(t.ros.astype("int"), "ruido_normal_SUELO_AGUA", "GTiff", gdal.Open(img1))
+
+exit()"""
+
+"""
 mu_c, sigma_c = getInitialParams(t.ros, t.tc)
 mu_nc, sigma_nc = getInitialParams(t.ros, t.tn,lower=True)
 
-print "Compute ratio"
 
-#t.ros = t.ros.flatten()
-print time.clock()-start_time
+pnorig, pcorig  = 1 , 1
+mnorig, mcorig  = 1 , 1
+snorig, scorig  = 1 , 1
 
-start_time = time.clock()
+pn ,pc  =  0.75, 0.25
 
-
-pn ,pc  =  0.5, 0.5
-
-for i in range(10):
+for i in range(100):
+	if (pcorig == pc and pnorig == pn and mcorig == mu_c and mnorig == mu_nc and scorig == sigma_c and snorig == sigma_nc):
+		break
+	
+	pcorig = pc
+	pnorig = pn
+	
+	mcorig = mu_c
+	mnorig = mu_nc
+	
+	scorig = sigma_c
+	snorig = sigma_nc
 
 	if i!=0:
 		sigma_c , sigma_nc = sqrt(sigma_c) , sqrt(sigma_nc)
 
 	Pn_Pxn_Px , Pc_Pxc_Px = t.computeRatio(pc, pn, mu_c, sigma_c, mu_nc, sigma_nc)
+	mu_c_orig,mu_nc_orig  =  mu_c , mu_nc
+
 	pn, pc =  t.equation1(Pn_Pxn_Px, Pc_Pxc_Px)
+
 	mu_nc, mu_c = t.equation2(Pn_Pxn_Px, Pc_Pxc_Px)
-	sigma_nc, sigma_c = t.equation3(Pn_Pxn_Px, Pc_Pxc_Px, mu_nc, mu_c)
+	sigma_nc, sigma_c = t.equation3(Pn_Pxn_Px, Pc_Pxc_Px, mu_nc_orig, mu_c_orig)
 
 	print (pn,pc)
 	print (mu_nc, mu_c)
 	print (sigma_nc, sigma_c)
+	print ""
 
+print "Roots"
+print computeRoots(mu_nc,mu_c,sigma_nc,sigma_c,pn,pc)
+"""
+exit()
+from sklearn.cluster import MeanShift, estimate_bandwidth
 
-print time.clock()-start_time
+ms = MeanShift(bandwidth=0.05)
+X = cambios.reshape(-1, 1)
+print X
+ms.fit(X)
+labels = ms.labels_
+cluster_centers = ms.cluster_centers_
+print cluster_centers
 
-
+labels_unique = np.unique(labels)
+n_clusters_ = len(labels_unique)
 
 """
-t.imageDiff(source='ndi')
+import matplotlib.pyplot as plt
+from itertools import cycle
 
-t.getMagnitudeMatrix()
-t.getAlphaMatrix()
-print np.mean(t.alpha), t.alpha.min(), t.alpha.max()
+plt.figure(1)
+plt.clf()
+
+colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+for k, col in zip(range(n_clusters_), colors):
+    my_members = labels == k
+    cluster_center = cluster_centers[k]
+    plt.plot(X[my_members, 0], col + '.')
+    plt.plot(cluster_center[0],  'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=14)
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+
 """
--- utils.py
+++ utils.py
@@ -67,13 +67,15 @@ def getNDIs(matrix):
 	res = np.empty([len(aux), x , y])
 
 	for i, (b1, b2) in enumerate(aux):
-
+		#print (b1, b2)
 		img1 = matrix[b1,:,:]
 		img2 = matrix[b2,:,:]
-		with np.errstate(invalid='ignore',divide='ignore'):
-			res[i,:,:]= (img1 - img2) / (img1 + img2)
+		a = (img1 - img2)
+		b = (img1 + img2)
+		
+		res[i,:,:]=  a / b
 
-	res[~np.isfinite(res)]= 0
+	#res[~np.isfinite(res)]= 0
 
 	return res
 
@@ -90,9 +92,9 @@ def pdf(matrix, mu, sigma):
 
 def computeRoots(mu_n, mu_c, var_n, var_c, Pn, Pc):
 	a = var_n - var_c
-	b = 2 ( (var_c*mu_n) - (var_n*mu_c)
-	
-	c1 = var_n * pow(mu_c,2)
+	b = 2 * ( (var_c*mu_n) - (var_n*mu_c) )
+
+	c1 = var_n*(mu_c**2)
 	c2 = -var_c*(mu_n**2)
 	c3 = -2*(var_c*var_n)* log( (sqrt(var_n)* Pc) / (sqrt(var_c)* Pn) )
 
@@ -106,28 +108,30 @@ def computeRoots(mu_n, mu_c, var_n, var_c, Pn, Pc):
 	else:
 		print "No hay raices reales"
 
-print computeRoots(0.012015065332291968, 0.032281068167829857, 1.347091345656299e-05, 0.00017052328317002674, 0.95092060861307237, 0.049079391386927629)
+#print computeRoots(0.081996954703117797, 0.29743607003638306,0.00032570313937767557, 0.0011796231862130817,0.45565179421797364, 0.54434820578202647)
 
 def test():
 	""" Matrix examples	"""
-	a = np.array([ [ [6,7,8] , [9,11,15] ] , [ [11,12,13] , [14,15,16] ] ])
-	b = np.array([ [ [1,2,3] , [4,5,6] ] , [ [3,2,1] , [6,5,4] ] ])
-	c = np.array([ [ [5,5,5] , [5,6,9] ] , [ [8,10,12] , [8,10,12] ] ])
-	d = np.sqrt(np.array([ [89,125,169] , [89,136,225] ]))
-	
-	dif = imageDiff(b,a)
-	print "Difference image"
-	print dif
-	print "Check"
-	print np.array_equal(dif, c)
-	ros = getMagnitudeMatrix(dif)
-	print "Ros matrix"
-	print ros
-	print np.array_equal(ros,d)
-"""if __name__ == "__main__":
-	a = np.array([ [ [6,7,8] , [9,11,15] ] , [ [11,12,13] , [14,15,16] ] ])
-	b = np.array([ [ [1,2,3] , [4,5,6] ] , [ [3,2,1] , [6,5,4] ] ])
-	print getNDIs(a).shape
-	print getNDIs(b).shape
-	
-"""
+	a = np.array([ [ [6,7,8] , [9,11,15] ] , [ [11,12,13] , [14,15,16] ]]).astype('float')
+	b = np.array([ [ [1,2,3] , [4,5,6] ] , [ [3,2,1] , [6,5,4] ] ]).astype('float')
+	c = np.array([ [ [5,5,5] , [5,6,9] ] , [ [8,10,12] , [8,10,12] ] ]).astype('float')
+	d = np.sqrt(np.array([ [89,125,169] , [89,136,225] ])).astype('float')
+
+	a = np.array([ [ [1,1.5,1.4] , [1.5,1.4,1] ]]).astype('float')
+
+	print pdf(a,0,1)
+	"""
+				dif = imageDiff(b,a)
+				print "Difference image"
+				print dif
+				print "Check"
+				print np.array_equal(dif, c)
+				ros = getMagnitudeMatrix(dif)
+				print "Ros matrix"
+				print ros
+				print np.array_equal(ros,d)
+			
+			"""
+
+if __name__ == "__main__":
+	test()
